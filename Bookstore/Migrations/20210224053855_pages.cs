69ded58606e1ad15a75d4a6062d1a4920d0c39dc
ShelbyR <70963984+srohn00@users.noreply.github.com>
2021-02-23 22:39:09 -0700

    pages migration

---
+++ Bookstore/Migrations/20210224053855_pages.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookstore.Migrations
+{
+    public partial class pages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: false),
+                    AuthorMiddle = table.Column<string>(nullable: true),
+                    AuthorLast = table.Column<string>(nullable: false),
+                    AuthorFirst = table.Column<string>(nullable: false),
+                    Publisher = table.Column<string>(nullable: false),
+                    ISBN = table.Column<string>(nullable: false),
+                    Classification = table.Column<string>(nullable: false),
+                    Category = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Pages = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
