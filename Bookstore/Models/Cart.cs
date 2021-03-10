e8b180e07e615f931dfe4c1c47ac874c3a0bfa0c
ShelbyR <70963984+srohn00@users.noreply.github.com>
2021-03-09 19:21:00 -0700

    part 2

---
+++ Bookstore/Models/Cart.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; } = new List<CartLine>();
+
+        public void AddItem (Book book, int qty)
+        {
+            CartLine line = Lines
+                .Where(b => b.Book.BookID == book.BookID)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                Lines.Add(new CartLine
+                {
+                    Book = book,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                line.Quantity += qty;
+            }    
+        }
+
+        public void RemoveLine(Book book) =>
+            Lines.RemoveAll(x => x.Book.BookID == book.BookID);
+
+        public void Clear() => Lines.Clear();
+
+        public decimal ComputeTotalSum() => Lines.Sum(e => e.Book.Price * e.Quantity);
+        
+        public class CartLine
+        {
+            public int CartLineID { get; set; }
+            public Book Book { get; set; }
+            public int Quantity { get; set; }
+        }
+    }
+}
--- Bookstore/Pages/Buy.cshtml.cs
+++ Bookstore/Pages/Buy.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bookstore.Infrastructure;
+using Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,32 @@ namespace Bookstore.Pages
 {
     public class BuyModel : PageModel
     {
-        public void OnGet()
+        private IBookstoreRepository repository;
+
+        //constructor
+        public BuyModel (IBookstoreRepository repo)
         {
+            repository = repo;
+        }
+
+        //properties
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+
+        //methods
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart()
+;        }
+
+        public IActionResult OnPost(long bookId, string returnUrl)
+        {
+            Book book = repository.Books.FirstOrDefault(b => b.BookID == bookId);
+
+            Cart = HttpContext.Session.SetJson<Cart>("cart", Cart);
+
+            return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
 }
